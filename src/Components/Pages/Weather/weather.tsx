55aada8d0c608d3e59e8c7ce1fd4fa92856f4fa7
Vasiliy <Wasyok.ru@yandex.ru>
2023-03-11 15:33:12 +0500

    store and main page

--- src/Components/Pages/Weather/weather.tsx
+++ src/Components/Pages/Weather/weather.tsx
@@ -1,14 +1,47 @@
 import { FC } from "react";
 import Navbar from "../../UI/Navbar/navbar";
-interface WeatherProps {
-  name?: string;
-}
-const Weather: FC = (props: WeatherProps) => {
-  return (
-    <>
-      <Navbar />
-    </>
+import { useGeolocated } from "react-geolocated";
+import { observer } from "mobx-react-lite";
+
+const Weather: FC = () => {
+  const { coords, isGeolocationAvailable, isGeolocationEnabled } =
+    useGeolocated({
+      positionOptions: {
+        enableHighAccuracy: false,
+      },
+      userDecisionTimeout: 5000,
+    });
+  return !isGeolocationAvailable ? (
+    <div>Your browser does not support Geolocation</div>
+  ) : !isGeolocationEnabled ? (
+    <div>Geolocation is not enabled</div>
+  ) : coords ? (
+    <table>
+      <tbody>
+        <tr>
+          <td>latitude</td>
+          <td>{coords.latitude}</td>
+        </tr>
+        <tr>
+          <td>longitude</td>
+          <td>{coords.longitude}</td>
+        </tr>
+        <tr>
+          <td>altitude</td>
+          <td>{coords.altitude}</td>
+        </tr>
+        <tr>
+          <td>heading</td>
+          <td>{coords.heading}</td>
+        </tr>
+        <tr>
+          <td>speed</td>
+          <td>{coords.speed}</td>
+        </tr>
+      </tbody>
+    </table>
+  ) : (
+    <div>Getting the location data&hellip; </div>
   );
 };
-
-export default Weather;
+export default observer(Weather);
---
+++ src/Store/store.ts
@@ -0,0 +1,62 @@
+import { makeAutoObservable } from "mobx";
+import {
+  getCurrentCityWeather,
+  getCurrentUserWeather,
+} from "../Components/API/serviceWeather";
+import { getWeekWeather } from "./../Components/API/serviceWeather";
+import { ICardWeather, ICities, IUserCities } from "./../Types/types";
+
+class Store {
+  userCity: IUserCities = {};
+  cities: ICities[] = [];
+  currentCity: ICardWeather = {};
+
+  constructor() {
+    makeAutoObservable(this);
+  }
+  //   получаем погоду в городе пользователя из его местоположения(долгота + ширина)
+  getCurrentUserWeather = async (lat: number, lon: number) => {
+    const { data } = await getCurrentUserWeather(lat, lon);
+    this.userCity = {
+      city: data.name,
+      temperature: Math.floor(data.main.temp - 273.15),
+      weather: data.weather[0].description,
+      icon: data.weather[0].icon,
+    };
+  };
+  //   получаем погоду в городе
+  getCurrentCityWeather = async (city: string) => {
+    const { data } = await getCurrentCityWeather(city);
+    this.cities = [
+      ...this.cities,
+      {
+        city: data.name,
+        temperature: Math.floor(data.main.temp - 273.15),
+        weather: data.weather[0].description,
+        id: data.id,
+        lat: data.coord.lat,
+        lon: data.coord.lon,
+        icon: data.weather[0].icon,
+      },
+    ];
+    localStorage.setItem("cities", JSON.stringify(this.cities));
+  };
+  //   получаем погоду за 7 дней опять же из координат
+  getWeekWeather = async (lat: number, lon: number) => {
+    const { data } = await getWeekWeather(lat, lon);
+    //форматирование времени из UTC UNIX в секундах
+    const format = (time: number): string => {
+      const day = new Date(time * 1000);
+      return day.toLocaleDateString();
+    };
+    this.currentCity = data.daily.map((day: any) => ({
+      dayTemp: day.temp.day,
+      nightTemp: day.temp.night,
+      weather: day.weather[0].description,
+      day: format(day.dt),
+      icon: day.weather[0].icon,
+    }));
+  };
+}
+
+export const store = new Store();
