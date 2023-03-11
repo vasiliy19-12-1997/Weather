984a98978a511fd1205f979b87bfcbb9a33a7ce8
Vasiliy <Wasyok.ru@yandex.ru>
2023-03-11 15:32:53 +0500

    service

--- src/Components/API/serviceWeather.ts
+++ src/Components/API/serviceWeather.ts
@@ -1,23 +1,18 @@
 import axios from "axios";
-import { Component } from "react";
 
-export default class serviceWeather extends Component {
-  Apikey = "d2cda96c7881426eff7d69f04226b87b";
-  instance = axios.create({
-    baseURL: "http://api.openweathermap.org/data/2.5/",
-  });
-  getCurrentUserWeather = (lat: number, lon: number) => {
-    return this.instance.get(
-      `weather?lat=${lat}&lon=${lon}&appid=${this.Apikey}`
-    );
-  };
-  getCurrentCityWeather = (city: string) => {
-    return this.instance.get(`weather?q=${city}&appid=${this.Apikey}`, {});
-  };
-  getWeekWeather = (lat: number, lon: number) => {
-    return this.instance.get(
-      `onecall?lat=${lat}&lon=${lon}&exclude=hourly,minutely,current&units=metric&appid=${this.Apikey}`,
-      {}
-    );
-  };
-}
+const Apikey = "d2cda96c7881426eff7d69f04226b87b";
+const instance = axios.create({
+  baseURL: "http://api.openweathermap.org/data/2.5/",
+});
+export const getCurrentUserWeather = (lat: number, lon: number) => {
+  return instance.get(`weather?lat=${lat}&lon=${lon}&appid=${Apikey}`);
+};
+export const getCurrentCityWeather = (city: string) => {
+  return instance.get(`weather?q=${city}&appid=${Apikey}`, {});
+};
+export const getWeekWeather = (lat: number, lon: number) => {
+  return instance.get(
+    `onecall?lat=${lat}&lon=${lon}&exclude=hourly,minutely,current&units=metric&appid=${Apikey}`,
+    {}
+  );
+};
--- src/Types/types.ts
+++ src/Types/types.ts
@@ -1,15 +1,19 @@
 export interface IUserCities {
   city?: string;
-  temperature?: string;
+  temperature?: number;
   weather?: string;
   icon?: string;
 }
 export interface ICities extends IUserCities {
-  //ширина
-  lat?: number;
-  //долгота
-  lon?: number;
+  lat?: number; //ширина
+  lon?: number; //долгота
   id?: number;
+}
+export interface ICardWeather extends IUserCities {
+  dayTemp?: number;
+  nightTemp?: number;
+  weather?: string;
+  day?: string;
 }
 export interface IAuth {
   isAuth: boolean;
