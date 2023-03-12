ce32500361e33e4052f6b23fc5a33f0b2c4b9598
Vasiliy <Wasyok.ru@yandex.ru>
2023-03-12 10:17:53 +0500

    weather

--- src/Components/Pages/Weather/weather.tsx
+++ src/Components/Pages/Weather/weather.tsx
@@ -1,9 +1,13 @@
-import { FC } from "react";
+import { FC, useEffect, useState } from "react";
 import Navbar from "../../UI/Navbar/navbar";
 import { useGeolocated } from "react-geolocated";
 import { observer } from "mobx-react-lite";
+import { store } from "../../../Store/store";
 
 const Weather: FC = () => {
+  const [lat, setLat] = useState(0);
+  const [lon, setLon] = useState(0);
+  const [loading, setLoading] = useState(true);
   const { coords, isGeolocationAvailable, isGeolocationEnabled } =
     useGeolocated({
       positionOptions: {
@@ -11,35 +15,40 @@ const Weather: FC = () => {
       },
       userDecisionTimeout: 5000,
     });
+  //получаем текущий город пользователя
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition((pos) => {
+      setLat(pos.coords.latitude);
+      setLon(pos.coords.longitude);
+      setLoading(false);
+    });
+  }, []);
+  //функция из стора, чтобы отобразить на странице
+  useEffect(() => {
+    store.getCurrentUserWeather(lat, lon);
+    setLoading(false);
+  }, [store.userCity.city]);
+
   return !isGeolocationAvailable ? (
     <div>Your browser does not support Geolocation</div>
   ) : !isGeolocationEnabled ? (
     <div>Geolocation is not enabled</div>
   ) : coords ? (
-    <table>
-      <tbody>
-        <tr>
-          <td>latitude</td>
-          <td>{coords.latitude}</td>
-        </tr>
-        <tr>
-          <td>longitude</td>
-          <td>{coords.longitude}</td>
-        </tr>
-        <tr>
-          <td>altitude</td>
-          <td>{coords.altitude}</td>
-        </tr>
-        <tr>
-          <td>heading</td>
-          <td>{coords.heading}</td>
-        </tr>
-        <tr>
-          <td>speed</td>
-          <td>{coords.speed}</td>
-        </tr>
-      </tbody>
-    </table>
+    <>
+      <Navbar />
+      <div>
+        {loading ? (
+          <div>{loading}</div>
+        ) : (
+          <div>
+            <p>{store.userCity.city}</p>
+            <p>{store.userCity.weather}</p>
+            <p>{store.userCity.temperature}</p>
+            <p>{store.userCity.temperature}</p>
+          </div>
+        )}
+      </div>
+    </>
   ) : (
     <div>Getting the location data&hellip; </div>
   );
