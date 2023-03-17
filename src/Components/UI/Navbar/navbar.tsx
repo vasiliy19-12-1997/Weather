c145b10da8dde1c293bb8c908d6865f6f7bfaa5e
Vasiliy <Wasyok.ru@yandex.ru>
2023-03-17 23:04:30 +0500

    fix burger menu

--- src/Components/UI/Navbar/navbar.tsx
+++ src/Components/UI/Navbar/navbar.tsx
@@ -1,5 +1,5 @@
-import { FC, useContext } from "react";
-import { useNavigate } from "react-router-dom";
+import { FC, useContext, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import { AuthContext } from "../../../Context/context";
 import { IAuth } from "../../../Types/types";
 import "./navbar.scss";
@@ -7,20 +7,34 @@ interface NavbarProps {}
 
 const Navbar: FC<NavbarProps> = () => {
   const { isAuth, setIsAuth } = useContext(AuthContext) as IAuth;
-
   const navigate = useNavigate();
   const logOut = (e: React.MouseEvent<HTMLButtonElement>) => {
     e.preventDefault();
     setIsAuth(false);
     localStorage.removeItem("auth");
   };
+
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const handleMenuClick = () => {
+    setMenuOpen(!menuOpen);
+  };
   return (
     <>
-      <div id="nav-icon6">
-        <span>
-          <div>1</div>
-          <div>2</div>
-        </span>
+      <div className="burger-menu-container">
+        <div
+          className={`burger-menu ${menuOpen ? "open" : ""}`}
+          onClick={handleMenuClick}
+        >
+          <div className="line1"></div>
+          <div className="line2"></div>
+          <div className="line3"></div>
+        </div>
+        <div className={`menu-items ${menuOpen ? "open" : ""}`}>
+          <a href="#">Search Weather</a>
+          <a href="#">Profile</a>
+          <a href="#">Log Out</a>
+        </div>
       </div>
     </>
   );
