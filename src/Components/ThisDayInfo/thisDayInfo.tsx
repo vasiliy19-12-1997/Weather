6c51ec7b636f8f685e6668708421a133de4d4c3a
Vasiliy <Wasyok.ru@yandex.ru>
2023-03-22 07:13:21 +0500

    styles for main page

--- src/Components/ThisDayInfo/thisDayInfo.tsx
+++ src/Components/ThisDayInfo/thisDayInfo.tsx
@@ -1,21 +1,35 @@
 import { SharedSvgSelector } from "../../Assets/Icons/Shared/sharedSvgSelector";
 import "./thisDayInfo.scss";
+import cloud from "../../Assets/Images/cloud.png";
 type Props = {};
 
 const ThisDayInfo = (props: Props) => {
   return (
     <div className="day-info">
       <div className="icons">
+        <SharedSvgSelector id="temperature" />
         <SharedSvgSelector id="Humidity" />
         <SharedSvgSelector id="Dew point" />
         <SharedSvgSelector id="wind" />
         <SharedSvgSelector id="pressure" />
+        <SharedSvgSelector id="Visibility" />
       </div>
-      <div className="text">
+      <div className="title">
+        <p className="temperature">temperature</p>
         <p className="Humidity">Humidity</p>
         <p className="Dew point">Dew point</p>
         <p className="wind">wind</p>
         <p className="pressure">pressure</p>
+        <p className="Visibility">Visibility</p>
+      </div>
+      <div className="text">
+        <p className="temperature">Feels like -3°C%</p>
+        <p className="Humidity">86%</p>
+        <p className="Dew point">-1°C</p>
+        <p className="wind"> 3.0m/s </p>
+        <p className="pressure">1012hPa</p>
+        <p className="Visibility">10.0km</p>
+        <img src={cloud} alt="cloud" />
       </div>
     </div>
   );
